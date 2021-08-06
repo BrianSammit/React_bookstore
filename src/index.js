00ea8e2a8c60fb8a1667a9f05dd016e45457c565
BrianSammit <briansammit.cruz@gmail.com>
2021-08-06 10:49:41 -0500

    Create filter

--- src/index.js
+++ src/index.js
@@ -4,10 +4,25 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
 import App from './components/App';
-import booksReducer from './reducers/books';
+import rootReducer from './reducers';
 
-const store = createStore(booksReducer);
+const initialState = {
+  books: [
+    {
+      id: 1,
+      title: 'See, Touch, Feel',
+      category: 'Kids',
+    },
+    {
+      id: 2,
+      title: 'The Sum of Us',
+      category: 'History',
+    },
+  ],
+};
 
+const store = createStore(rootReducer, initialState);
+store.getState();
 ReactDOM.render(
   <Provider store={store}>
     <App />
