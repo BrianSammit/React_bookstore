fe0c96f5ecb1ebcad95e4b4fbfb2ed277036b2eb
BrianSammit <briansammit.cruz@gmail.com>
2021-08-06 11:27:46 -0500

    Add style to CategoryFilter

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './CategoryFilter.css';
 
 const CategoryFilter = ({ handleFilterChange, categories }) => (
-  <select onChange={handleFilterChange}>
-    <option value="all">All</option>
-    {categories.map((category) => (
-      <option key={category}>{category}</option>
-    ))}
-  </select>
+  <div className="panel-bg">
+    <div className="filter">
+      <h1 className="Bookstore-CMS">Bookstore CMS</h1>
+      <div className="cat">
+        <h3 className="BOOKS">BOOKS</h3>
+        <select className="CATEGORIES" onChange={handleFilterChange}>
+          <option value="all">CATEGORIES</option>
+          {categories.map((category) => (
+            <option key={category}>{category}</option>
+          ))}
+        </select>
+      </div>
+    </div>
+    <div className="Oval">
+      <div className="Mask" />
+    </div>
+  </div>
 );
 
 CategoryFilter.propTypes = {
